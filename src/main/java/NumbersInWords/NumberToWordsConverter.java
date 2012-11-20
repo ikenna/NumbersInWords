78b2e398a902dd95bd2843a8f4228cbeb831d5ee
Ikenna Nwaiwu <ikenna@memjure.com>
2012-11-20 23:32:20 +0000

    twenties

--- src/main/java/NumbersInWords/NumberToWordsConverter.java
+++ src/main/java/NumbersInWords/NumberToWordsConverter.java
@@ -1,8 +1,51 @@
 package NumbersInWords;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumberToWordsConverter {
-    public String convert(int i) {
-        return "one";
+    private final Map<Integer, String> wordMap;
+
+    public NumberToWordsConverter() {
+        wordMap = new HashMap<Integer, String>();
+        wordMap.put(1, "one");
+        wordMap.put(2, "two");
+        wordMap.put(3, "three");
+        wordMap.put(4, "four");
+        wordMap.put(5, "five");
+        wordMap.put(6, "six");
+        wordMap.put(7, "seven");
+        wordMap.put(8, "eight");
+        wordMap.put(9, "nine");
+        wordMap.put(10, "ten");
+        wordMap.put(11, "eleven");
+        wordMap.put(12, "twelve");
+        wordMap.put(13, "thirteen");
+        wordMap.put(14, "fourteen");
+        wordMap.put(15, "fifteen");
+        wordMap.put(16, "sixteen");
+        wordMap.put(17, "seventeen");
+        wordMap.put(18, "eighteen");
+        wordMap.put(19, "nineteen");
+        wordMap.put(20, "twenty");
+
+    }
+
+    public String convert(Integer number) {
+        if (isBetween20And30(number)) {
+            return wordsBetween20And30(number);
+        } else {
+            return wordMap.get(number);
+        }
+    }
+
+    private boolean isBetween20And30(Integer number) {
+        return 20 < number && number < 30;
+    }
+
+    private String wordsBetween20And30(Integer number) {
+        int remainder = number % 10;
+        return "twenty " + wordMap.get(remainder);
     }
 }
--- src/test/java/NumbersInWords/NumberToWordsConverterTest.java
+++ src/test/java/NumbersInWords/NumberToWordsConverterTest.java
@@ -1,6 +1,5 @@
 package NumbersInWords;
 
-import org.hamcrest.Matcher;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -10,8 +9,38 @@ public class NumberToWordsConverterTest {
 
     @Test
     public void testShouldConvertNumberToWords() throws Exception {
+        assertNumberInWords(1, "one");
+        assertNumberInWords(2, "two");
+        assertNumberInWords(3, "three");
+        assertNumberInWords(4, "four");
+        assertNumberInWords(5, "five");
+        assertNumberInWords(6, "six");
+        assertNumberInWords(7, "seven");
+        assertNumberInWords(8, "eight");
+        assertNumberInWords(9, "nine");
+        assertNumberInWords(10, "ten");
+        assertNumberInWords(11, "eleven");
+        assertNumberInWords(12, "twelve");
+        assertNumberInWords(13, "thirteen");
+        assertNumberInWords(14, "fourteen");
+        assertNumberInWords(15, "fifteen");
+        assertNumberInWords(16, "sixteen");
+        assertNumberInWords(17, "seventeen");
+        assertNumberInWords(18, "eighteen");
+        assertNumberInWords(19, "nineteen");
+        assertNumberInWords(20, "twenty");
 
-        String actual = new NumberToWordsConverter().convert(1);
-        assertThat(actual, is("one"));
+        assertNumberInWords(21, "twenty one");
+        assertNumberInWords(22, "twenty two");
+        assertNumberInWords(23, "twenty three");
+        assertNumberInWords(24, "twenty four");
+        assertNumberInWords(29, "twenty nine");
+    }
+
+    // test should give error if cannot work out number or number not within range
+
+    private void assertNumberInWords(int number, String numberInWords) {
+        String actual = new NumberToWordsConverter().convert(number);
+        assertThat(actual, is(numberInWords));
     }
 }
